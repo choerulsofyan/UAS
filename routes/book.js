fc0960bee81fc22c5fccb4e5a2b9e8b30b171ecc
madungit remote origigit config --global user.name madun <ilmankori@gmail.com>
2019-07-17 21:30:27 +0700

    add Route, Controller Book & define route book to entry point (index.js)

---
+++ routes/book.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const router = express.Router();
+
+const bookController = require('../controllers/book');
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+router.get('/', bookController.getIndexBook);
+// get one book
+router.get('/:book_id', bookController.getDetailBook);
+// post book POST /api/book gets JSON bodies
+router.post('/', urlencodedParser, bookController.storeBook);
+// update book
+router.post('/:book_id', urlencodedParser, bookController.updateBook);
+// delete book
+router.post('/:book_id/destroy', urlencodedParser, bookController.destroyBook);
+
+module.exports = router;
