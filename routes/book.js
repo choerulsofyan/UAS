9e0cba4276cef5da1691058bfd033616547a714b
madungit remote origigit config --global user.name madun <ilmankori@gmail.com>
2019-07-20 13:03:59 +0700

    update middleware book store, update, delete + User store, update, delete

--- routes/book.js
+++ routes/book.js
@@ -6,6 +6,8 @@ const router = express.Router();
 
 const bookController = require('../controllers/book');
 
+const auth = require('../configs/auth');
+
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
@@ -13,11 +15,11 @@ router.get('/', bookController.getIndexBook);
 // get one book
 router.get('/:book_id', bookController.getDetailBook);
 // post book POST /api/book gets JSON bodies
-router.post('/', urlencodedParser, bookController.storeBook);
+router.post('/', urlencodedParser, auth.verifyToken, bookController.storeBook);
 // update book
-router.post('/:book_id', urlencodedParser, bookController.updateBook);
+router.post('/:book_id', urlencodedParser, auth.verifyToken, bookController.updateBook);
 // delete book
-router.post('/:book_id/destroy', urlencodedParser, bookController.destroyBook);
+router.post('/:book_id/destroy', urlencodedParser, auth.verifyToken, bookController.destroyBook);
 
 // search book by judul
 router.post('/search/:judul', urlencodedParser, bookController.searchBook);
--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,8 @@ const router = express.Router();
 
 const userController = require('../controllers/user');
 
+const auth = require('../configs/auth');
+
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
@@ -13,13 +15,16 @@ router.get('/', userController.getAllUser);
 // get one user
 router.get('/:user_id', userController.getDetailUser);
 // post user POST /api/user gets JSON bodies
-router.post('/', urlencodedParser, userController.storeUser);
+router.post('/register', urlencodedParser, auth.verifyToken, userController.storeUser); // only role = 1 can access (admin)
 // update user
-router.put('/:user_id', urlencodedParser, userController.updateUser);
+router.put('/:user_id', urlencodedParser, auth.verifyToken, userController.updateUser); // only role = 1 can access (admin)
 // delete user
-router.delete('/:user_id/destroy', urlencodedParser, userController.destroyUser);
+router.delete('/:user_id/destroy', urlencodedParser, auth.verifyToken, userController.destroyUser); // only role = 1 can access (admin)
 
 // search user by judul
 router.post('/search/:username', urlencodedParser, userController.searchUser);
+
+// login 
+router.post('/login', urlencodedParser, userController.loginUser);
 
 module.exports = router;
